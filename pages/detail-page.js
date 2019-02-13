8ce6b379f5a4b0df88d6f365f48f6e0375e84ab2
Teeeep <casperjantepe@gmail.com>
2019-02-13 18:33:17 +0100

    set up form validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,15 +1,67 @@
+function capitalizeFirstChar(name) {
+
+    
+    let firstLetter = name.charAt(0).toUpperCase() 
+    console.log(firstLetter)
+    return firstLetter + name.slice(1)
+}
+
+function doesNotPassAllValidations (name, msg) {
+    if (!name) {
+        alert('You forgot to fill in your name!')
+        return true
+    }
+
+    if (!msg) {
+        alert('You forgot to enter your message!')
+        return true
+
+    }
+    if (msg.length > 280) {
+        alert('Your comment is too long')
+        return true
+    }
+    return false
+}
+
+// eventhandler
 function submitComment() {
+
+    // data gathering
     const inputField = document.getElementById('name')
-    const name = inputField.value 
+    let name = inputField.value 
     const textArea = document.getElementById('msg')
     const msg = textArea.value
+
+    if (doesNotPassAllValidations(name, msg)) {
+        return null
+    }
+
+    name = capitalizeFirstChar(name)
+
+    // create elements
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
+
+    // adjust elememts
     h3.innerHTML = `${name} said:`
     p.innerHTML = msg
     comment.classList.add('comment')
     comment.appendChild(h3)
     comment.appendChild(p)
-    console.log('comment')
-}
+    
+    // display elements
+    const commentSection = document.getElementById('comments')
+    commentSection.appendChild(comment)
+
+
+    // reset values
+    inputField.value = null
+    textArea.value = null
+
+    
+} 
+
+
+
